345ca00d482b0889142eb3df693f7a355b33d902
brickoyster <dimitris2002keramidas@gmail.com>
2025-05-23 13:05:45 +0300

    add server & client functionality

--- pr2/client.c
+++ pr2/client.c
@@ -14,7 +14,7 @@ int INTERVAL = 2;
 int main(int argc, char const* argv[]) {
     // Parse command line options
     int argopt;
-    while ((argopt = getopt(argc, (char * const *)argv, "a:p:b:i:")) != -1) {
+    while ((argopt = getopt(argc, (char * const *)argv, "a:p:b:i:hH?")) != -1) {
         switch (argopt) {
             case 'a':
                 SERVER_IP = optarg;
@@ -28,11 +28,59 @@ int main(int argc, char const* argv[]) {
             case 'i':
                 INTERVAL = atoi(optarg);
                 break;
+            case 'h' || 'H' || '?':
+                printf("Usage: %s [-a ip] [-p port] [-b buffer_size] [-i interval]\n", argv[0]);
+                exit(EXIT_SUCCESS);
             default:
-                fprintf(stderr, "Usage: %s [-p port] [-b buffer_size] [-d duration] [-i interval]\n", argv[0]);
+                fprintf(stderr, "Usage: %s [-a ip] [-p port] [-b buffer_size] [-i interval] [-h]/[-H]/[-?]\n", argv[0]);
                 exit(EXIT_FAILURE);
         }
     }
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[BUFFER_SIZE];
+    memset(buffer, 'A', sizeof(buffer));
+    
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation error");
+        return -1;
+    }
 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, SERVER_IP, &serv_addr.sin_addr) <= 0) {
+        perror("Invalid address/ Address not supported");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("Connection Failed");
+        return -1;
+    }
+
+    printf("Connected to server. Starting SpeedTest...\n");
+
+    time_t start = time(NULL), now;
+    size_t interval_bytes = 0, total_bytes = 0;
+
+    while (1) {
+        now = time(NULL);
+        ssize_t bytes = send(sock, buffer, BUFFER_SIZE, 0);
+        if (bytes <= 0) break;
+
+        interval_bytes += bytes;
+        total_bytes += bytes;
+
+        if ((now - start) % INTERVAL == 0 && interval_bytes > 0) {
+            printf("\r[+%ld sec] Sent: %.2f Mbits (%.2f Mbits in %d seconds)          ", now - start, (total_bytes * 8.0) / 1048576.0, (interval_bytes * 8.0) / 1048576.0, INTERVAL);
+            fflush(stdout);
+            interval_bytes = 0;
+            sleep(1);  // throttle reporting
+        }
+    }
+
+    printf("\n");
+    close(sock);
     return 0;
 }
--- pr2/server.c
+++ pr2/server.c
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netdb.h>
 #include <time.h>
 
 int PORT = 5001;
@@ -51,5 +52,76 @@ int main(int argc, char const* argv[]) {
     int opt = 1;
     setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
+    // Attaching socket to the port
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;  // not localhost
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to the address
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("Bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Start listening for incoming connections
+    listen(server_fd, 1);
+
+    char hostbuffer[256];
+    char *IPbuffer;
+    struct hostent *host_entry;
+    int hostname;
+
+    // To retrieve hostname
+    if ((hostname = gethostname(hostbuffer, sizeof(hostbuffer)) == -1)) {
+        perror("Error while getting hostname.");
+        exit(EXIT_FAILURE);
+    }
+
+    // To retrieve host information
+    if ((host_entry = gethostbyname(hostbuffer)) == NULL) {
+        perror("Error while getting host entry.");
+        exit(EXIT_FAILURE);
+    }
+
+    // address into ASCII string
+    IPbuffer = inet_ntoa(*((struct in_addr*) host_entry->h_addr_list[0]));
+    printf("Server %s listening on %s:%d...\n", hostbuffer, IPbuffer, PORT);
+
+    // Main loop
+    while (1) {
+        // Accept a new connection
+        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        printf("Client connected!\n");
+
+        time_t start = time(NULL), now;
+        size_t total_bytes = 0, interval_bytes = 0;
+        
+        while ((now = time(NULL)) - start < EXPERIMENT_DURATION) {
+            // Receive data from the client
+            ssize_t bytes = recv(new_socket, buffer, BUFFER_SIZE, 0);
+            if (bytes <= 0) break; // Connection closed or error
+
+            total_bytes += bytes;
+            interval_bytes += bytes;
+            
+            // Print throughput every INTERVAL seconds
+            if ((now - start) % INTERVAL == 0 && interval_bytes > 0) {
+                // Calculate throughput in Mbps
+                // 1 byte = 8 bits, 1 Mbit = 1,048,576 bits
+                double throughput = (interval_bytes * 8.0) / (1048576.0 * INTERVAL);
+                double agg_throughput = (total_bytes * 8.0) / (1048576.0 * (now - start));
+                
+                printf("\r[+%ld sec] Throughput: %.2f Mbps, Total Throughput: %.2f Mbps          ", now - start, throughput, agg_throughput);
+                fflush(stdout);
+                
+                interval_bytes = 0;
+                sleep(1);  // prevent multiple prints within 1 second
+            }
+        }
+        printf("\n");
+        close(new_socket);
+    }
+
+    close(server_fd);
     return 0;
 }
